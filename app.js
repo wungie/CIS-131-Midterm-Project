d9c1c8ca0dc43baf4731411ba44c2782bfdf2927
Alex Enge <alexace2002@gmail.com>
2022-10-15 13:32:35 -0500

    added dice + small changes

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 
-chanceAry = ['good', 'bad']
+//chanceAry = ['good', 'bad']
 
 window.addEventListener('load', function(){
     //this.alert("Hola")
 })
+
+
+document.getElementById("dice").addEventListener("click", rollDie);
+
+
+function rollDie(){
+    //console.log("roll!") // Used for testing
+    roll = Math.ceil(Math.random() * 12);
+    console.log(roll);
+    var display = document.getElementById("die-sum");
+    display.innerHTML = roll
+    
+
+}
+
+function classGrade(){
+    console.log("grade func");
+
+}
